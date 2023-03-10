32f1f87ee587a73bbbae27e21c56e47e90700b84
Qanit <muhammadhijran17@gmail.com>
2023-03-10 10:42:12 +0700

    commit v1.2

--- tugaspabw1.py
+++ tugaspabw1.py
@@ -45,3 +45,26 @@ for i in range(1):
     orang.append(data_orang)
 
 print(orang)
+
+#no 3
+
+orang = [
+    {'nama': 'Arsene Lupin', 'nik': 1234, 'jenis_kelamin': 'male', 'tanggal_lahir': '1902-03-23'},
+    {'nama': 'Sherlock Holmes', 'nik': 4321, 'jenis_kelamin': 'male', 'tanggal_lahir': '1876-08-16'},
+    {'nama': 'Irene Adler', 'nik': 6789, 'jenis_kelamin': 'female', 'tanggal_lahir': '1884-10-07'}
+]
+
+
+nama_dicari = input("Masukkan nama yang ingin dicari: ")
+
+
+for data in orang:
+    if data['nama'].lower() == nama_dicari.lower():
+        print(f"Data ditemukan:")
+        print(f"Nama: {data['nama']}")
+        print(f"NIK: {data['nik']}")
+        print(f"Jenis kelamin: {data['jenis_kelamin']}")
+        print(f"Tanggal lahir: {data['tanggal_lahir']}")
+        break
+else:
+    print("Data tidak ada")
